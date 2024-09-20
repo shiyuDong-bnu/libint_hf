9b8c57be4ec5af8881ba2a8f0c488ac1d558ee65
Dong-Shiyu-NJU <2656304384@qq.com>
2024-09-20 10:24:07 +0800

    new compile script

--- compile_hf.sh
+++ compile_hf.sh
@@ -1,3 +1,5 @@
-g++ hf.cc -g  -o hf.o  -I$HOME/opt/linalg/eigen3/include/eigen3 -I$HOME/opt/boost/include -I$HOME/opt/quantum_chem/libint2/include -fPIC \
+g++ hf_pure.cc -g  -o hf_pure.o  -I$HOME/opt/linalg/eigen3/include/eigen3 -I$HOME/opt/boost/include -I$HOME/opt/quantum_chem/libint_t4/include -fPIC \
                        -I$HOME/opt/linalg/xtensor/include -I$HOME/opt/linalg/xtl/include \
-                       -L$HOME/opt/quantum_chem/libint2/lib -lint2 
+                       -I$HOME/opt/linalg/xtensor-blas/include/ \
+                       -L$HOME/opt/quantum_chem/libint_t4/lib -lint2 \
+                       -L/home/sydong/opt/linalg/openblas/lib/ -lopenblas 
